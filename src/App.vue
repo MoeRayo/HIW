f82df0e4c8b7988cf45ff38727dd16b1420278a2
moerayo <moronfolu.motunrayo1@gmail.com>
2023-04-25 01:50:39 +0100

    feat: creating the header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
-import TheNavigation from '@/components/TheNavigation.vue'
+import { RouterLink } from "vue-router";
+import TheNavigation from "@/components/TheNavigation.vue";
 </script>
 
 <template>
   <header>
     <div class="">
-      <TheNavigation/>
-      <nav>
-
-        <RouterLink to="/">Home</RouterLink>
-        
-      
-      </nav>
+      <TheNavigation />
     </div>
   </header>
 
   <RouterView />
 </template>
-
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import TheHeader from "@/components/TheHeader.vue";
 </script>
 
 <template>
   <main>
+    <TheHeader />
   </main>
 </template>
